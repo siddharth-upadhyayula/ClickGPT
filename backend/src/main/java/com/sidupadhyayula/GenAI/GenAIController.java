b7ff30c8dde3975efa63aa1c88cf59e831f6af98
Sai Siddharth Upadhyayula <usaisiddarth@gmail.com>
2024-08-31 15:55:40 -0400

    Rearrange files

---
+++ backend/src/main/java/com/sidupadhyayula/GenAI/GenAIController.java
@@ -0,0 +1,49 @@
+package com.sidupadhyayula.GenAI;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.ai.image.ImageResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+public class GenAIController {
+
+    ChatService chatService;
+    ImageService imageService;
+
+    public GenAIController(ChatService chatService, ImageService imageService){
+        this.chatService = chatService;
+        this.imageService = imageService;
+    }
+
+    @GetMapping("ask-ai")
+    public String getResponse(@RequestParam String prompt){
+        return chatService.getResponse(prompt);
+    }
+
+    @GetMapping("ask-ai-options")
+    public String getResponseOptions(@RequestParam String prompt){
+        return chatService.getResponseOptions(prompt);
+    }
+
+    @GetMapping("generate-image")
+    public List<String> generateImages(HttpServletResponse response,
+                                       @RequestParam String prompt,
+                                       @RequestParam(defaultValue = "hd") String quality,
+                                       @RequestParam(defaultValue = "4") int n,
+                                       @RequestParam(defaultValue = "1024") int width,
+                                       @RequestParam(defaultValue = "1024") int height) throws IOException {
+        ImageResponse imageResponse = imageService.generateImage(prompt, quality, n, width, height);
+
+        // Streams to get urls from ImageResponse
+        List<String> imageUrls = imageResponse.getResults().stream()
+                .map(result -> result.getOutput().getUrl())
+                .toList();
+
+        return imageUrls;
+    }
+}
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      const [activeTab, setActiveTab] = useState('image-generator');
+    </div>
+  );
+}
+
+export default App;
